a1859e888fa11175410ae6f41d388e5e6d89023b
Ankit Gupta <126098830+ankitguptabk@users.noreply.github.com>
2024-11-26 13:23:18 +0530

    Time: 18 ms (47.4%), Space: 37.5 MB (45.99%) - LeetHub

---
+++ 3318-find-x-sum-of-all-k-long-subarrays-i/3318-find-x-sum-of-all-k-long-subarrays-i.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+
+int solve(unordered_map<int,int>&mp,int x){
+    vector<pair<int,int>>ans;
+    for(auto &i:mp){
+        ans.push_back({i.second,i.first});        
+    }
+    int res=0;
+    auto cmp=[](auto a,auto b){
+        if(a.first==b.first)return a.second>b.second;
+        return a.first>b.first;        
+    };
+    sort(ans.begin(),ans.end(),cmp);
+    for(int i=0;i<ans.size();i++){
+        res+=(ans[i].first)*ans[i].second;
+        x-=1;
+        if(x==0)break;
+    }
+    return res;
+}
+    vector<int> findXSum(vector<int>& n, int k, int x) {
+        vector<int>ans(n.size()-k+1);
+         for(int i=0;i<n.size()-k+1;i++){
+            unordered_map<int,int>mp;
+            for(int j=i;j<k+i;j++){               
+                mp[n[j]]++;
+            } 
+            int f=solve(mp,x);
+            ans[i]=f;
+         }
+         return ans;
+    }
+};
