256ae06b896ee7ce0ac1e54024cb9a89f8634668
Ankit Gupta <126098830+ankitguptabk@users.noreply.github.com>
2024-12-02 23:41:15 +0530

    Time: 0 ms (100%), Space: 9.7 MB (18.9%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string gcdOfStrings(string s, string a) {
+        int x=s.size();
+        int y=a.size();
+        int ans=__gcd(x,y);
+        string b="";
+        for(int i=0;i<ans;i++){
+            b+=s[i];
+        }
+        string c="", d="";
+        int s_repeat=s.size()/b.size();
+        int a_rep=a.size()/b.size();
+        for(int i=0;i<s_repeat;i++){
+            c+=b;
+        }
+        for(int i=0;i<a_rep;i++){
+            d+=b;
+        }
+        if(c==s && d==a) return b;
+        return "";
+    }
+};
