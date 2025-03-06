08099cf6ecc145e8f59976c8cacec75237c6bf82
Ankit Gupta <126098830+ankitguptabk@users.noreply.github.com>
2025-03-07 02:18:04 +0530

    Time: 35 ms (54.36%), Space: 80.1 MB (99.14%) - LeetHub

---
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxIceCream(vector<int>& c, int co) {
+        int ans=0;
+        sort(c.begin(),c.end());
+        for(int i=0;i<c.size();i++){
+            if(co<c[i]){
+                break;
+            }
+            else{
+                co-=c[i];
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
