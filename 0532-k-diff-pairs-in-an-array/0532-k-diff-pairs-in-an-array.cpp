6c4914def30c111e2b3bf2d660c5e6ad16d1676e
Ankit Gupta <126098830+ankitguptabk@users.noreply.github.com>
2025-03-10 14:05:25 +0530

    Time: 8 ms (59.11%), Space: 18.7 MB (13.2%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int findPairs(vector<int>& n, int k) {
-        sort(n.begin(),n.end());
-        set<pair<int,int>>uniquep;
+        unordered_set<int>seen,unique;
         int ans=0;
-        for(int i=0;i<n.size()-1;i++){
-            for(int j=i+1;j<n.size();j++){
-                if(abs(n[i]-n[j])==k){
-                    pair<int,int>p={n[i],n[j]};
-                    if(uniquep.find(p)==uniquep.end()){
-                        uniquep.insert(p);
-                        ans++;
-                    }
-                }
+        for(int i=0;i<n.size();i++){
+            if(seen.count(n[i]-k)){
+                unique.insert(n[i]-k);
             }
+            if(seen.count(n[i]+k)){
+                unique.insert(n[i]);
+            }
+            seen.insert(n[i]);
         }
+        ans=unique.size();
         return ans;
     }
 };
