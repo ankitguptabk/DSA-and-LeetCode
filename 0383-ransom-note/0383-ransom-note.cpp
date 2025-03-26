a2a45b4a7ac55a8ac6f0462fe40edffd19ca011a
Ankit Gupta <126098830+ankitguptabk@users.noreply.github.com>
2025-03-26 22:33:47 +0530

    Time: 8 ms (23%), Space: 12 MB (10.25%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool canConstruct(string r, string m) {
+        sort(r.begin(),r.end());
+        sort(m.begin(),m.end());
+        unordered_map<char,int>a,b;
+        for(auto c:r){
+            a[c]++;
+        }
+        for(auto c:m){
+            b[c]++;
+        }
+        for(auto const x:b){
+            for(auto const y:a){
+                if(x.first==y.first && x.second<y.second) return false;
+            }
+        }
+        for(auto const y:a){
+            if(b.find(y.first)==b.end()) return false;
+        }
+        return true;
+    }
+};
