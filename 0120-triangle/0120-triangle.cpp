7cd04d0c13e6eef4fa2b91ded409e0b1c84faad5
Ankit Gupta <126098830+ankitguptabk@users.noreply.github.com>
2025-04-02 00:07:02 +0530

    Time: 0 ms (100%), Space: 12.6 MB (81.63%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-    int minimumTotal(vector<vector<int>>& t) {
-        int ans=t[0][0];
+    int minimumTotal(vector<vector<int>>& t){
         for(int i=t.size()-2;i>=0;i--){
             for(int j=0;j<t[i].size();j++){
                 t[i][j]+=min(t[i+1][j],t[i+1][j+1]);
