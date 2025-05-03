95c7063f21d20bae7c964fc18bf1ef3d50e6624d
Ankit Gupta <126098830+ankitguptabk@users.noreply.github.com>
2025-05-03 16:00:24 +0530

    Time: 3 ms (64.78%), Space: 11.9 MB (32.69%) - LeetHub

---
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> charac;
+        for (auto x : s) {
+            charac.push(x);
+            if (charac.size() >= 3) {
+                char c = charac.top(); charac.pop();
+                char b = charac.top(); charac.pop();
+                char a = charac.top(); charac.pop();
+                if (a != 'a' || b != 'b' || c != 'c') {
+                    charac.push(a);
+                    charac.push(b);
+                    charac.push(c);
+                }
+            }
+        }
+        return charac.empty();
+    }
+};
