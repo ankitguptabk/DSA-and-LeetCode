915da0bbd4d184330e4bacb9e14aac3280912750
Ankit Gupta <126098830+ankitguptabk@users.noreply.github.com>
2025-05-13 01:39:32 +0530

    Time: 1321 ms (45.22%), Space: 580.8 MB (11.17%) - LeetHub

---
+++ 3527-find-the-most-common-response/3527-find-the-most-common-response.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string findCommonResponse(vector<vector<string>>& res) {
+        unordered_map<string,int>m;
+        vector<vector<unordered_set<string>>> r(res.size());
+        for(int i=0;i<res.size();i++){
+            unordered_set<string>s(res[i].begin(),res[i].end());
+            r[i].push_back(s);
+        }
+        for(int i=0;i<r.size();i++){
+            for(int j=0;j<r[i].size();j++){
+                for(auto const x:r[i][j]){
+                    m[x]++;
+                }
+            }
+        }
+        int b=INT_MIN;
+        string ans="zzzzzzzzzz";
+        for (const auto& a : m){
+            if (a.second > b || (a.second == b && a.first<ans)) {
+                b = a.second;
+                ans = a.first;
+            }
+        }
+        return ans;
+    }
+};
