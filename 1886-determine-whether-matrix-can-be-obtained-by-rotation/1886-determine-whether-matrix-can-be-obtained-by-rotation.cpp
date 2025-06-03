1cc49dd92393eb71923dd60ac80db6f863a81d1a
Ankit Gupta <126098830+ankitguptabk@users.noreply.github.com>
2025-06-03 20:13:33 +0530

    Time: 0 ms (100%), Space: 14.9 MB (11.69%) - LeetHub

--- 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.cpp
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.cpp
@@ -6,8 +6,8 @@ public:
             if(m==t) return true;
             vector<vector<int>>tempo=m;
             for(int i=0;i<m.size();i++){
-                for(int j=m[0].size()-1;j>=0;j--){
-                    m[j][m.size()-1-i]=tempo[i][j];
+                for(int j=0;j<m[0].size();j++){
+                    m[i][j]=tempo[m[0].size()-1-j][i];
                 }
             }
             x++;
