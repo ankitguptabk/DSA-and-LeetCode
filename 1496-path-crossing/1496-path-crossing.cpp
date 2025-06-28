5e77e1158c25108bd738a51f5616de8c0e438cdd
Ankit Gupta <126098830+ankitguptabk@users.noreply.github.com>
2025-06-28 14:57:31 +0530

    Time: 0 ms (100%), Space: 9.5 MB (78.78%) - LeetHub

--- 1496-path-crossing/1496-path-crossing.cpp
+++ 1496-path-crossing/1496-path-crossing.cpp
@@ -11,8 +11,6 @@ public:
             else if(p[i]=='W') x--;
             if(vis.count({x,y})) return true;
             vis.insert({x,y});
-            if( i+1<p.size() && ((p[i]=='N' && p[i+1]=='S') || (p[i]=='S' && p[i+1]=='N') || (p[i]=='E' && p[i+1]=='W') || (p[i]=='W' && p[i+1]=='E') )) return true;
-            else if(i+3 <p.size() && (p[i] !=p[i+1] && p[i+1]!=p[i+2] && p[i+2]!=p[i]&& p[i+2]!=p[i+3] && p[i+3]!=p[i+1] && p[i+3]!=p[i])) return true; 
         }
         return false;
     }
