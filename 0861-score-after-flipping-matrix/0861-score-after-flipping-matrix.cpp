4acf14e1ca783e14117fc3ed17fd208bc68d843e
Ankit Gupta <126098830+ankitguptabk@users.noreply.github.com>
2025-07-14 01:47:50 +0530

    Time: 0 ms (100%), Space: 11.2 MB (71.56%) - LeetHub

---
+++ 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int matrixScore(vector<vector<int>>& g) {
+        for(int i=0;i<g.size();i++){
+            if(g[i][0]!=1){
+                for(int j=0;j<g[0].size();j++){
+                    if(g[i][j]==1){
+                        g[i][j]=0;
+                    }
+                    else g[i][j]=1;
+                }
+            }
+        }
+        for(int i=0;i<g[0].size();i++){
+            int one=0;
+            for(int j=0;j<g.size();j++){
+                if(g[j][i]==1){
+                    one++;
+                }
+            }
+            if(g.size()-one>one){
+                for(int j=0;j<g.size();j++){
+                    if(g[j][i]==1){
+                        g[j][i]=0;
+                    }
+                    else g[j][i]=1;
+                }
+            }
+        }
+        long long ans=0;
+         for(int i=0;i<g.size();i++){
+            long long rowval=0;
+            for(int j=0;j<g[0].size();j++){
+                rowval=(rowval<<1) | g[i][j];
+            }
+            ans+=rowval;
+         }
+         return static_cast<int>(ans);
+    }
+};
