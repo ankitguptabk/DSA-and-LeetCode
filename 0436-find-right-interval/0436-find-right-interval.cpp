82ef8ae9b8c756536b40ad8a1255a9a227ab5f7b
Ankit Gupta <126098830+ankitguptabk@users.noreply.github.com>
2025-07-24 04:56:53 +0530

    Time: 7 ms (74.64%), Space: 30 MB (55.38%) - LeetHub

--- 0436-find-right-interval/0436-find-right-interval.cpp
+++ 0436-find-right-interval/0436-find-right-interval.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
     vector<int> findRightInterval(vector<vector<int>>& a) {
-        vector<int>ans;
+        vector<int> ans;
+        vector<pair<int,int>> b;
         for(int i=0;i<a.size();i++){
-            int x=INT_MAX;
-            for(int j=0;j<a.size();j++){
-                if(a[j][0]>=a[i][1]){
-                    x=min(x,a[j][0]);
+            b.push_back({a[i][0],i});
+        }
+        sort(b.begin(),b.end());
+        for(int i=0;i<a.size();i++){
+            int t=a[i][1];
+            int l=0,r=b.size()-1;
+            int x=INT_MAX,y=-1;
+            while(l<=r){
+                int m=(l+r)/2;
+                if(b[m].first>=t){
+                    x=b[m].first;
+                    y=b[m].second;
+                    r=m-1;
+                }
+                else{
+                    l=m+1;
                 }
             }
-            if(x==INT_MAX){
-                ans.push_back(-1);
-            }
-            else if(x!=INT_MAX){
-                for(int j=0;j<a.size();j++){
-                    if(x==a[j][0]){
-                        ans.push_back(j);
-                        break;
-                    }
-                }
-            }
+            if(x==INT_MAX) ans.push_back(-1);
+            else ans.push_back(y);
         }
         return ans;
     }
