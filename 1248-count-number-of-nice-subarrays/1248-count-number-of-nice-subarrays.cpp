eb5eb81b559b4b67323e71cdffe6da5f7701a4e9
Ankit Gupta <126098830+ankitguptabk@users.noreply.github.com>
2025-07-24 06:59:36 +0530

    Time: 3 ms (88.48%), Space: 73.8 MB (30.4%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -5,8 +5,8 @@ public:
         deque<int> deq= {-1};
         for(int i = 0; i<n.size(); i++) {
             if(n[i] % 2 == 1) deq.push_back(i);
-            if(deq.size() > k + 1) deq.pop_front();
-            if(deq.size() == k + 1) ans+= deq[1]-deq[0];
+            if(deq.size() > k+1) deq.pop_front();
+            if(deq.size() == k+1) ans+= deq[1]-deq[0];
         }
         return ans;
     }
