e89660d27275ed2c4c613ce7f5149f18269be31a
Ankit Gupta <126098830+ankitguptabk@users.noreply.github.com>
2025-07-25 01:35:04 +0530

    Time: 23 ms (60.16%), Space: 8 MB (22.85%) - LeetHub

--- 1753-maximum-score-from-removing-stones/1753-maximum-score-from-removing-stones.cpp
+++ 1753-maximum-score-from-removing-stones/1753-maximum-score-from-removing-stones.cpp
@@ -2,16 +2,12 @@ class Solution {
 public:
     int maximumScore(int a, int b, int c) {
         int ans=0;
-        int x=max(a,max(b,c));
-        int y=min(a,min(b,c));
-        int z=a+b+c-(x+y);
-        while((x>0 && y>0) || (x>0 && z>0) || (y>0 && z>0)){
-            int p=x,q=y,r=z;
-            x=max({p,q,r});
-            y=min({p,q,r});
-            z=p+q+r-(x+y);
-            x--;
-            z--;
+        vector<int>x={a,b,c};
+        while((x[0]>0 && x[1]>0) || (x[0]>0 && x[2]>0) || (x[1]>0 && x[2]>0)){
+            sort(x.begin(),x.end());
+            int p=x[2],q=x[1];
+            p--,q--;
+            x[2]=p,x[1]=q;
             ans++;
         }
         return ans;
