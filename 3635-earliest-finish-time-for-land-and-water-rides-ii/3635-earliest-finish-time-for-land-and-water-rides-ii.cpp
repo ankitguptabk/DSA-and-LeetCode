82e20f5544a9f550d197dd8aa24de648a092afb8
Ankit Gupta <126098830+ankitguptabk@users.noreply.github.com>
2025-08-04 02:55:42 +0530

    Time: 11 ms (83.33%), Space: 238.1 MB (100%) - LeetHub

--- 3635-earliest-finish-time-for-land-and-water-rides-ii/3635-earliest-finish-time-for-land-and-water-rides-ii.cpp
+++ 3635-earliest-finish-time-for-land-and-water-rides-ii/3635-earliest-finish-time-for-land-and-water-rides-ii.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration,
                            vector<int>& waterStartTime, vector<int>& waterDuration) {
-        int ans = INT_MAX;
-        int landMinEnd = INT_MAX;
-        for (int i = 0; i < landStartTime.size(); i++) {
-            int landEnd = landStartTime[i] + landDuration[i];
-            landMinEnd = min(landMinEnd, landEnd);
+        int ans=INT_MAX;
+        int landMinEnd=INT_MAX;
+        for (int i = 0; i< landStartTime.size(); i++) {
+            int landEnd=landStartTime[i] + landDuration[i];
+            landMinEnd= min(landMinEnd, landEnd);
         }
 
-        for (int i = 0; i < waterStartTime.size(); i++) {
+        for (int i=0;i<waterStartTime.size();i++) {
             int start = max(landMinEnd, waterStartTime[i]);
             int endTime = start + waterDuration[i];
             ans = min(ans, endTime);
         }
 
-        int waterMinEnd = INT_MAX;
+        int waterMinEnd=INT_MAX;
         for (int i = 0; i < waterStartTime.size(); i++) {
             int waterEnd = waterStartTime[i] + waterDuration[i];
             waterMinEnd = min(waterMinEnd, waterEnd);
@@ -26,7 +26,6 @@ public:
             int endTime = start + landDuration[i];
             ans = min(ans, endTime);
         }
-
         return ans;
     }
 };
