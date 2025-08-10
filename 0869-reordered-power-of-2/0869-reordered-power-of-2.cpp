1587a682b7d625a95841e6579a7e88cf6b2de61b
Ankit Gupta <126098830+ankitguptabk@users.noreply.github.com>
2025-08-10 17:15:15 +0530

    Time: 0 ms (100%), Space: 9 MB (51.03%) - LeetHub

---
+++ 0869-reordered-power-of-2/0869-reordered-power-of-2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool reorderedPowerOf2(int n) {
+        if(n==1) return true;
+        string s=to_string(n);
+        sort(s.begin(),s.end());
+        int a=s.size();
+        int maxpow=log2(1e9);
+        for(int i=0;i<=maxpow;i++){
+            int x=1<<i;
+            string p=to_string(x);
+            if(p.size()!=a) continue;
+            sort(p.begin(),p.end());
+            if(p==s) return true;
+        }
+        return false;
+    }
+};
