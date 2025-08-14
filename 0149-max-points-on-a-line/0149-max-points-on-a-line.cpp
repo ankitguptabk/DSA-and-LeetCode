51ed7c261a91dd57d575afd3e543e0afd67cfb77
Ankit Gupta <126098830+ankitguptabk@users.noreply.github.com>
2025-08-14 23:49:40 +0530

    Time: 17 ms (88.97%), Space: 17.7 MB (48.9%) - LeetHub

---
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxPoints(vector<vector<int>>& p) {
+        int n = p.size(), ans = 0;
+        if (n < 3) return n;
+        for (int i = 0; i < n; i++) {
+            unordered_map<double,int> m;
+            int dup = 1, vert = 0;
+            for (int j = i + 1; j < n; j++) {
+                if (p[i][0] == p[j][0] && p[i][1] == p[j][1]) dup++;
+                else if (p[i][0] == p[j][0]) vert++;
+                else m[(double)(p[i][1]-p[j][1])/(double)(p[i][0]-p[j][0])]++;
+            }
+            int mx = vert;
+            for (auto &it : m) mx = max(mx, it.second);
+            ans = max(ans, mx + dup);
+        }
+        return ans;
+    }
+};
