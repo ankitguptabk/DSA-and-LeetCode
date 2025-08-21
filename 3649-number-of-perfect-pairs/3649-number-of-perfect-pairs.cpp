526e03bf8888d6ae01bb7f02f0a30fcb11a673b5
Ankit Gupta <126098830+ankitguptabk@users.noreply.github.com>
2025-08-21 17:24:27 +0530

    Time: 58 ms (76.54%), Space: 137.7 MB (58.76%) - LeetHub

---
+++ 3649-number-of-perfect-pairs/3649-number-of-perfect-pairs.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    long long perfectPairs(vector<int>& nums) {
+        long long ans = 0;
+        int n = nums.size();
+        vector<int> num(n);
+        for (int i = 0; i < n; i++) {
+            num[i] = abs(nums[i]);
+        }
+        sort(num.begin(), num.end());
+        int j = n-2;
+        for (int i = n - 1; i >= 0; i--) {
+            while (j >= 0 && num[i] - num[j] <= num[j]) {
+                j--;
+            }
+            ans += i-j-1;
+        }
+
+        return ans;
+    }
+};
