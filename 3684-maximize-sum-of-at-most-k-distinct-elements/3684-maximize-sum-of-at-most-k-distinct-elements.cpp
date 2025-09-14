314716174b26582e368471586e83d316c48f872c
Ankit Gupta <126098830+ankitguptabk@users.noreply.github.com>
2025-09-14 12:58:58 +0530

    Time: 4 ms (70%), Space: 29.2 MB (30%) - LeetHub

--- 3684-maximize-sum-of-at-most-k-distinct-elements/3684-maximize-sum-of-at-most-k-distinct-elements.cpp
+++ 3684-maximize-sum-of-at-most-k-distinct-elements/3684-maximize-sum-of-at-most-k-distinct-elements.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     vector<int> maxKDistinct(vector<int>& nums, int k) {
-        unordered_set<int>s(nums.begin(),nums.end());
+        set<int>s(nums.begin(),nums.end());
         vector<int>n(s.begin(),s.end());
-        sort(n.begin(),n.end(),greater<int>());
         if(k>n.size()) k=n.size();
-        vector<int>ans(n.begin(),n.begin()+k);
+        vector<int>ans(n.end()-k,n.end());
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
