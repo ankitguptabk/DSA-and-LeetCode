24bc9f6345a5c097272f280feb34c4ac153eef8d
Ankit Gupta <126098830+ankitguptabk@users.noreply.github.com>
2025-09-15 21:24:13 +0530

    Time: 78 ms (100%), Space: 134.9 MB (100%) - LeetHub

--- 3679-minimum-discards-to-balance-inventory/3679-minimum-discards-to-balance-inventory.cpp
+++ 3679-minimum-discards-to-balance-inventory/3679-minimum-discards-to-balance-inventory.cpp
@@ -3,21 +3,17 @@ public:
     int minArrivalsToDiscard(vector<int>& a, int w, int m) {
         int ans = 0;
         vector<bool> f(a.size(), false);
-        unordered_map<int, int> map;
-        for (int i = 0; i < a.size(); i++) {
-            int l = i - w;
-            if (l >= 0 && !f[l]) {
+        unordered_map<int,int> map;
+        for (int i=0; i < a.size(); i++) {
+            int l=i - w;
+            if (l>=0 && !f[l]) {
                 map[a[l]]--;
-                if (map[a[l]] == 0) {
-                    map.erase(a[l]);
-                }
             }
             map[a[i]]++;
             if (!f[i] && map[a[i]] > m) {
                 f[i] = true;
                 ans++;
                 map[a[i]]--;
-                if (map[a[i]] == 0) map.erase(a[i]);
             }
         }
         return ans;
