8ca88eb3deae9ea34f0222cd52e213c4fd087d4c
Ankit Gupta <126098830+ankitguptabk@users.noreply.github.com>
2025-09-21 15:18:44 +0530

    Time: 0 ms (100%), Space: 103.8 MB (0%) - LeetHub

--- 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.cpp
+++ 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     long long maxTotalValue(vector<int>& n, int k) {
-        long long ans=0,a=LLONG_MAX,b=LLONG_MIN;
+        long long a=LLONG_MAX,b=LLONG_MIN;
         for(int i=0;i<n.size();i++){
             a=min(a,(long long) n[i]);
             b=max(b,(long long) n[i]);
         }
-        ans=b-a;
-        return ans*(long long)k;
+        return (b-a)*(long long)k;
     }
 };
