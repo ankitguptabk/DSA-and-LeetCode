c1f7d8e0a1787c32f12cdf7918d5b4b18b4c875c
Ankit Gupta <126098830+ankitguptabk@users.noreply.github.com>
2025-09-28 01:58:24 +0530

    Time: 16 ms (50%), Space: 184.5 MB (12.5%) - LeetHub

--- 3693-climbing-stairs-ii/3693-climbing-stairs-ii.cpp
+++ 3693-climbing-stairs-ii/3693-climbing-stairs-ii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int climbStairs(int n, vector<int>& c) {
         long long ans = 0;
-        vector<long long>dp(n+1,1e9);
+        vector<long long>dp(n+1, 1e9);
         dp[0]=0;
         for(int i=1;i<=n;i++){
             if(i-1>=0) dp[i]=min(dp[i],dp[i-1]+c[i-1]+1);
