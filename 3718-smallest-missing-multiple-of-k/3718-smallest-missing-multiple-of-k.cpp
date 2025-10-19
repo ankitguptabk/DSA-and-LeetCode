7ca3bf6d9b6b8cec3fbe4c1278496a5b5d514afd
Ankit Gupta <126098830+ankitguptabk@users.noreply.github.com>
2025-10-20 00:34:52 +0530

    Time: 0 ms (100%), Space: 24.8 MB (38.46%) - LeetHub

---
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int missingMultiple(vector<int>& n, int k) {
+        set<int>x;
+        int y=INT_MIN;
+        for(int i=0;i<n.size();i++){
+            y=max(y,n[i]);
+            if(n[i]%k==0){
+                x.insert(n[i]);
+            }
+        }
+        vector<int>a(x.begin(),x.end());
+        vector<int>b;
+        for(int i=1;i<=y+k;i++){
+            if(i%k==0){
+                b.push_back(i);
+            }
+        }
+        if(a.size()==0) return b[0];
+        for(int i=0;i<b.size();i++){
+            if(i<a.size() && b[i]!=a[i]) return b[i];
+            if(i>=a.size() && i<b.size()) return b[i];
+        }
+        return b[b.size()-1];
+    }
+};
