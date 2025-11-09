83e4916e7e863f1484d76dedea8e09476e9903c3
Ankit Gupta <126098830+ankitguptabk@users.noreply.github.com>
2025-11-09 17:22:49 +0530

    Time: 170 ms (92.86%), Space: 225 MB (100%) - LeetHub

--- 3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.cpp
+++ 3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
     int minimumDistance(vector<int>& n) {
-        int sz = (int)n.size();
-        unordered_map<int, array<int,3>> d;
-        int ans = INT_MAX;
+        int sz=(int)n.size(), ans=INT_MAX;
 
-        for(int i=0;i<sz;i++){
-            int v=n[i];
-            array<int,3> &a = d[v];
-            if(a[0]==0){
-                a[0] = 1;
-                a[1] = i;
-            }
-            else if(a[0]==1){
-                a[0] = 2;
-                a[2] = i;
-            }
-            else{
-                int dist = 2*(i - a[1]);
-                if(dist < ans) ans = dist;
-                a[1] = a[2];
-                a[2] = i;
+        vector<pair<int,int>> v(sz);
+        for(int i=0; i<sz; i++) v[i]={n[i],i};
+        sort(v.begin(),v.end());
+
+        for(int i=0;i<sz-2;i++){
+            int a=v[i].first,b=v[i+1].first,c=v[i+2].first;
+            int x=v[i].second,y=v[i+1].second,z=v[i+2].second;
+            if(a==b && b==c){
+                int d=abs(x-y)+abs(y-z)+abs(x-z);
+                if(d<ans) ans=d;
             }
         }
-        if(ans == INT_MAX) return -1;
+        if(ans==INT_MAX) return -1;
         return ans;
     }
 };
