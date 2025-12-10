8e109bec4f31fd2fb6fe49cdb7bb9ff72c1ceab4
Ankit Gupta <126098830+ankitguptabk@users.noreply.github.com>
2025-12-11 01:05:23 +0530

    Time: 1731 ms (9.27%), Space: 491.7 MB (5.04%) - LeetHub

---
+++ 3766-minimum-operations-to-make-binary-palindrome/3766-minimum-operations-to-make-binary-palindrome.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool palin(string s){
+        for(int i=0;i<s.size()/2;i++){
+            if(s[i]!=s[s.size()-1-i]) return false;
+        }
+        return true;
+    }
+    vector<int> minOperations(vector<int>& n) {
+        vector<int>ans;
+        for(int i=0;i<n.size();i++){
+            int x=n[i];
+            int g=INT_MAX,f=x,b=x;
+            bool a=false,d=false;
+            while(a==false){
+                string c=bitset<32>(f).to_string();
+                int p=c.find('1');
+                c=c.substr(p);
+                if(palin(c)){
+                    a=true;
+                }
+                else f++;
+            }
+            while(d==false && b>=1){
+                string c=bitset<32>(b).to_string();
+                int p=c.find('1');
+                c=c.substr(p);
+                if(palin(c)){
+                    d=true;
+                }
+                else b--;
+            }
+            g=min(g,(x-b));
+            g=min(g,(f-x));
+            ans.push_back(g);
+        }
+        return ans;
+    }
+};
