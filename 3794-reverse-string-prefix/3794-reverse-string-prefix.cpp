412b6695541ba934b5e461083fb36b11f9d6e173
Ankit Gupta <126098830+ankitguptabk@users.noreply.github.com>
2026-01-04 00:21:10 +0530

    Time: 0 ms (100%), Space: 8.9 MB (24.27%) - LeetHub

---
+++ 3794-reverse-string-prefix/3794-reverse-string-prefix.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    string reversePrefix(string s, int k) {
+        int n=s.size();
+        for(int i=0;i<k/2;i++){
+            swap(s[i],s[k-1-i]);
+        }
+        return s;
+    }
+};
