84902c001177b45832ac6c73e18f1cfc526347fb
Ankit Gupta <126098830+ankitguptabk@users.noreply.github.com>
2026-01-07 04:13:39 +0530

    Time: 35 ms (54.66%), Space: 9.9 MB (79.42%) - LeetHub

--- 2947-count-beautiful-substrings-i/2947-count-beautiful-substrings-i.cpp
+++ 2947-count-beautiful-substrings-i/2947-count-beautiful-substrings-i.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
     int beautifulSubstrings(string s, int k) {
         int ans=0;
-        set<char>v={'a','e','i','o','u'};
         for(int i=0;i<s.size();i++){
             int x=0,y=0;
             for(int j=i;j<s.size();j++){
-                if(v.find(s[j])!=v.end()){
+                if(s[j]=='a'|| s[j]=='e'|| s[j]=='i'|| s[j]=='o'|| s[j]=='u'){
                     x++;
                 }
                 else y++;
