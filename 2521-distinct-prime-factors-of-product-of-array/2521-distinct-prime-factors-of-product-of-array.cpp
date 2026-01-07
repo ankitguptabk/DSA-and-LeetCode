ce73ab1253a54a6ec20ea92b0450067d9b9906c9
Ankit Gupta <126098830+ankitguptabk@users.noreply.github.com>
2026-01-08 03:23:50 +0530

    Time: 7 ms (81.54%), Space: 22.8 MB (51.92%) - LeetHub

---
+++ 2521-distinct-prime-factors-of-product-of-array/2521-distinct-prime-factors-of-product-of-array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int distinctPrimeFactors(vector<int>& nums) {
+        unordered_set<int> used;
+        for (int x:nums) {
+            if (x % 2==0) {
+                used.insert(2);
+                while (x % 2 == 0) x /= 2;
+            }
+
+            for (int i=3;i*i<= x; i+=2) {
+                if (x%i==0) {
+                    used.insert(i);
+                    while (x % i == 0) x /= i;
+                }
+            }
+            if (x>1) {
+                used.insert(x);
+            }
+        }
+        return used.size();
+    }
+};
