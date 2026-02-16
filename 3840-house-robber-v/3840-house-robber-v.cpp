44db07eb3a1f64352d113b0c2c7e1a6ac227349c
Ankit Gupta <126098830+ankitguptabk@users.noreply.github.com>
2026-02-17 01:03:19 +0530

    Time: 4 ms (82.61%), Space: 234.7 MB (8.7%) - LeetHub

--- 3840-house-robber-v/3840-house-robber-v.cpp
+++ 3840-house-robber-v/3840-house-robber-v.cpp
@@ -3,24 +3,19 @@ public:
     long long rob(vector<int>& nums, vector<int>& colors) {
         long long ans=0;
         int n=nums.size();
-        if(n==3){
-           if(colors[0]==colors[1] && colors[0]==colors[2]){
-            ans=max(nums[0],nums[1]);
-            ans=max(ans,(long long)nums[2]);
-            return ans;
-           }
-        }
-        for(int i=0;i<n;i++){
-            int curr=nums[i];
+        for(int i=0;i<n;  ){
             int col=colors[i];
-            if(i+1<n && colors[i+1]==col){
-                curr=max(curr, nums[i+1]);
+            long long prev1=0, prev2=0;
+            while(i<n && colors[i]==col){
+                long long take=prev2+nums[i];
+                long long skip=prev1;
+                long long curr=max(take, skip);
+                prev2=prev1;
+                prev1=curr;
                 i++;
             }
-            ans+=curr;
+            ans+=prev1;
         }
-        vector<int> num = {10,15,10,10,15,10}, color = {1,1,1,1,1,1};
-        if(nums==num && colors==color ) return 35;
         return ans;
     }
 };
