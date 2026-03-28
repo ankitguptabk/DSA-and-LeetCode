6e99d1d14ac329a6af16d6fa5df99b642112d62f
Ankit Gupta <126098830+ankitguptabk@users.noreply.github.com>
2026-03-29 01:05:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3880-minimum-absolute-difference-between-two-values/3880-minimum-absolute-difference-between-two-values.cpp
+++ 3880-minimum-absolute-difference-between-two-values/3880-minimum-absolute-difference-between-two-values.cpp
@@ -3,12 +3,15 @@ public:
     int minAbsoluteDifference(vector<int>& nums) {
         int n=nums.size();
         int x=INT_MAX;
-        for(int i=0;i<n-1;i++){
-            for(int j=1;j<n;j++){
-                if((nums[i]==1 && nums[j]==2) || (nums[i]==2 && nums[j]==1)){
-                    x=min(x, abs(i-j));
-                }
+        int y=0,z=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==1){
+                y=i;
             }
+            if(nums[i]==2){
+                z=i;
+            }
+            if(y!=0 && z!=0) x=min(x,abs(y-z));
         }
         if(x==INT_MAX) return -1;
         return x;
