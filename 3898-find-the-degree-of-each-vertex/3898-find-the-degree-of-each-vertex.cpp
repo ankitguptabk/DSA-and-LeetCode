676db3262b3d5a4846f75c0fec7eee0fe7113193
Ankit Gupta <126098830+ankitguptabk@users.noreply.github.com>
2026-04-12 19:56:42 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3898-find-the-degree-of-each-vertex/3898-find-the-degree-of-each-vertex.cpp
+++ 3898-find-the-degree-of-each-vertex/3898-find-the-degree-of-each-vertex.cpp
@@ -6,9 +6,7 @@ public:
         for(int i=0;i<n;i++){
             int c=0;
             for(int j=0;j<n;j++){
-                if(matrix[i][j]==1){
-                    c++;
-                }
+                c+=matrix[i][j];
             }
             ans.push_back(c);
         }
